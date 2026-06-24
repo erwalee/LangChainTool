0b4d9d50b8d0c54205f4791fa51a8ce791740f07
erwalee <erwalee@ucsc.edu>
2026-06-23 17:57:54 -0700

    Uncomment

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const model = new ChatOpenRouter({
 });
 
 
-/* Chain Example 1 - Gaming
+// Chain Example 1 - Gaming
 const step1 = await model.invoke("If I am buying a computer which is better between Ryzen and Intel CPUs ")
 const step2 = await model.invoke(
     `If I instead build a PC using: "${step1.content}". 
@@ -21,7 +21,6 @@ console.log("Better CPU:", step1.content);
 console.log("Pros and Cons:", step2.content);
 console.log("Cost effective:", step3.content);
 
-*/
 // Plan, then Execute
 const planReponse = await model.invoke(`As an AI helper, a user wants to find the
     best chocolate chip cookie.
